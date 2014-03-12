b9928ccfbaa8193e931c49f9f1cbe40f7833a0cf
Nicholas Whitehead <nwhitehead@heliosdev.org>
2014-03-12 09:12:54 -0400

    DevSynch

--- controllers/connect/connect-directive.js
+++ controllers/connect/connect-directive.js
@@ -1,79 +1,133 @@
+ 
+
 
 //chrome.app.runtime.onLaunched.addListener(function(launchData) {
 console.info("Initializing Connect Modal....");
 // tsdbConsoleApp.module('connectModal',['ui.bootstrap','dialogs'])
 tsdbConsoleApp.controller('connectModal',function($scope,$rootScope,$timeout,$modal){
-  $scope.knownConnections = [
+  $scope.knownConnectionsX = [
     {id: 34, url: 'ws://localhost/tsdbws', isDefault: false},
-    {id: 35, url: 'ws://localhost:4242/tsdbws', isDefault: true}
+    
   ];
 
-  $scope.open = function () {
+    $scope.open = function () {
+      var modalInstance = null;
+      loadKnownConnections($scope.knownConnectionsX).then(function(conns){
+        modalInstance = $modal.open({
+          templateUrl: '/controllers/connect/connect.html',
+          controller: ModalInstanceCtrl,
+          resolve: {
+            knownConnections: function () {
+              console.debug("RETURNING $scope.knownConnections: [%O]", $scope.knownConnections);              
+              return $scope.knownConnections;
+            }
+          }
+        });
+        modalInstance.result.then(
+          // Something was selected
+          function (selectedItem) {
+            $scope.selected = selectedItem;
+            console.info('Modal Instance Selected: [%O]', selectedItem);
+            storeConnection(selectedItem);
+          }, 
+          // Dialog was dismissed
+          function () {
+            console.info('Selected: [%O], Modal dismissed at: [%s]', $scope.selected,  new Date());
+          });
 
-    var modalInstance = $modal.open({
-      templateUrl: '/controllers/connect/connect.html',
-      controller: ModalInstanceCtrl,
-      resolve: {
-        knownConnections: function () {
-          loadKnownConnections();
-          return $scope.knownConnections;
-        }
-      }
     });
 
 
-    modalInstance.result.then(
-      // Something was selected
-      function (selectedItem) {
-        $scope.selected = selectedItem;
-        console.info('Modal Instance Selected: [%O]', selectedItem);
-      }, 
-      // Dialog was dismissed
-      function () {
-        console.info('Selected: [%O], Modal dismissed at: [%s]', $scope.selected,  new Date());
-      });
   };
 
   var connectionDb = new IDBStore({
-    dbVersion: 2,
+    dbVersion: 1,
     storeName: 'connections',
     keyPath: 'id',
     autoIncrement: true,
     onStoreReady: function(){
       console.log('Connection Store Ready');
+      connectionDb.getAll(function(data) {console.info("ALL DATA: %O", data)});
     },
     indexes: [
       { name: 'url', keyPath: 'url', unique: true, multiEntry: false }
     ]
-  });    
+  }); 
 
-  var loadKnownConnections = function() {
+  var switchDefault = function(id) {
+    console.info("Setting default connection to [%s]", id);
+    //connectionDb.
+  }
+
+  var storeConnection = function(kConn) {
+    console.info("Storing [%O]...", kConn);
+    var foundRecord = null;
+    var onItem = function (item) {
+      console.log('got item: [%O]', item);
+      foundRecord = item;
+    };
+    var finishStore = function() {
+      if(foundRecord==null) {
+        var onsuccess = function(id){
+          console.log('Saved new Connection with ID: [%s]' , id);
+        }
+        var onerror = function(error){
+          console.error('Connection save failure', error);
+        }         
+        connectionDb.put(kConn, onsuccess, onerror);        
+      } else {
+        if(kConn.isDefault &&  !foundRecord.isDefault) {
+          switchDefault(foundRecord.id);
+        }
+      }
+    }
+    var keyRange = connectionDb.makeKeyRange({
+      lower: kConn.url,
+      upper: kConn.url
+    });
+    connectionDb.iterate(onItem, {
+      index: 'url',
+      keyRange: keyRange,
+      onEnd: finishStore
+    });
+
+  }   
+
+  var loadKnownConnections = function(cArr) {
+    var d = $.Deferred();
     console.info("Fetching all known connections...")
-    var kcCount = 0;
+    var kcCount = 0;    
+    console.info("KC Before: [%O]", cArr);
     connectionDb.getAll(function(data){
       data.forEach(function(kc){
         kcCount++;
         console.info("Retrieved KC: [%O]", kc);
-        knownConnections.push(kc);
-      });      
-      console.info("Cached [%s] KnownConnections", kcCount);
+        //$scope.knownConnections.push(kc);
+        cArr.push(kc);
+      });    
+      d.resolve($scope.knownConnections);  
+      console.info("Cached [%s] KnownConnections ---> [%O]", kcCount, cArr);
     }, onerror);
+    return d.promise();
   }
 
 
 });
 var ModalInstanceCtrl = function ($scope, $modalInstance, knownConnections) {
+  console.info("Modal loading. Scope: [%O], Modal: [%O], KC:[%O]", $scope, $modalInstance, knownConnections);
   $scope.knownConnections = knownConnections;
   $scope.selected = null;
+  $scope.f = {d: false};
 
-  //{    
-  //  knownConnection: $scope.knownConnections[0]
-  //};
-  //console.info("SELECTED: [%O]", $scope.selected.knownConnection);
+  console.info("Modal loaded. KC:[%O]", $scope.knownConnections);
 
   $scope.ok = function () {
-
-    $modalInstance.close($scope.selected.knownConnection);
+    var selectedConnection = {
+      url : $scope.f.url.toLowerCase(),
+      isDefault : $scope.f.d
+    };
+    console.info("Selected [%O]", selectedConnection);
+    $modalInstance.close(selectedConnection);    
   };
 
   $scope.cancel = function () {
