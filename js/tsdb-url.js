603c80dd032ae68332d1af6f895cb6350b28fb56
Nicholas Whitehead <nwhitehead@heliosdev.org>
2014-06-27 17:04:15 -0400

    DevSync

--- js/tsdb-url.js
+++ js/tsdb-url.js
@@ -1,23 +1,11 @@
  function OpenTSDBURL(urlstring) {
- 	var url = new URL(encodeURI(urlstring));
- 	// grab all the non query URL items
- 	this.host = url.host;
- 	this.hostname  = url.hostname;
- 	this.origin = url.origin;
- 	this.pathname = url.pathname;
- 	this.port = url.port;
- 	this.protocol = url.protocol;
- 	// extract the full query path and decode
- 	this.query = decodeQuery(url);
- 	console.info("Query: [  " + this.query + "  ]");
-
  	// Performs a manual decode of the query path
  	// (that is, everything after the '?')
  	// since decodeURI seems to not decode many of the symbols
  	// used by OpenTSDB URLs
- 	OpenTSDBURL.prototype.decodeQuery = function(url) {
+ 	OpenTSDBURL._decodeQuery = function(url) {
  		if(url.search=="" || url.search=="?") return "";
- 		return url
+ 		return url.search
  			.replace(/%20/g, " ")
  			.replace(/%7B/g, "{")
  			.replace(/%7D/g, "}")
@@ -27,8 +15,120 @@
  			.replace(/%257B/g, "{")
  			.replace(/%257D/g, "}")
             .replace(/%255B/g, "[")
-            .replace(/%255D/g, "]"); 			
+            .replace(/%255D/g, "]")
+            .substring(1)	
  	}
+
+ 	// Determines if the passed string appears to be encoded
+ 	// by scanning for a few common encoded symbols
+ 	OpenTSDBURL._isEncoded = function(urlstring) {
+ 		if(urlstring==null) return false;
+ 		var encoded =  
+ 			urlstring.indexOf('%20') != -1 ||
+ 			urlstring.indexOf('%7B') != -1 ||
+ 			urlstring.indexOf('%5B') != -1;
+ 		console.info("Encoded: [%s]", encoded);
+ 		return encoded;
+ 	}
+
+ 	// Custom splitter since TSDB parameters can have embedded '=' in them.
+ 	OpenTSDBURL._splitByFirst = function(value, delim) {
+ 		if(value==null || value=='') return [];
+ 		var idx = value.indexOf(delim);
+ 		if(idx==-1) return [];
+ 		return [
+ 			value.substring(0, idx),
+ 			value.substring(idx + delim.length)
+ 		];
+ 	}
+
+ 	// parses a metric segment such as "avg:1m-avg:rate{counter,10,20}:sys.cpu{cpu=*,type=combined,host=PP-WK-NWHI-01}"
+ 	OpenTSDBURL.prototype._parseMetric = function(m) {
+ 		console.group("Parsing metric definition [%s]", m);
+ 		try {
+	 		var metric = {};
+	 		var rateCounterIdx = m.indexOf(":rate{");
+	 		console.info("Has Rate Counter: %s", rateCounterIdx!=-1);
+	 		var idx = m.indexOf("{", rateCounterIdx==-1, 0 : rateCounterIdx);
+	 		if(idx == -1) {
+	 			throw "No tag starter found in metric definition [" + m + "]";
+	 		}
+	 		var prefixes = m.substring(0, idx).split();
+	 		if(prefixes.length < 2) {
+	 			throw "Failed to parse metric prefixes. Less than 2 prefixes in  metric definition [" + m + "]";
+	 		}
+	 		if(prefixes.length > 4) {
+	 			throw "Failed to parse metric prefixes. More than 4 prefixes in  metric definition [" + m + "]";
+	 		}
+	 		switch(prefixes.length) {
+	 			case 4: 			
+	 				metric.name=prefixes[3];
+	 				metric.rate=prefixes[2];
+	 				metric.downsample=prefixes[1];
+	 				metric.aggregator=prefixes[0];
+	 				break;
+	 			case 3:     /// can be avg:rate:sys.cpu{cpu=*,type=combined,host=PP-WK-NWHI-01}    OR    avg:1m-avg:sys.cpu{cpu=*,type=combined,host=PP-WK-NWHI-01}
+	 				metric.aggregator=prefixes[0];
+	 				metric.name=prefixes[2];
+	 				if(prefixes[1].indexOf("rate")!=-1) {
+	 					metric.rate=prefixes[1];
+	 				} else {
+	 					metric.downsample=prefixes[1];
+	 				}
+	 				break;
+	 			case 2:
+	 				metric.name=prefixes[1];
+	 				metric.aggregator=prefixes[0];
+	 		}	
+	 		
+
+
+
+
+	 	} finally {
+	 		console.groupEnd();
+	 	}
+
+ 	}
+
+
+
+ 	var url = new URL(OpenTSDBURL._isEncoded(urlstring) ? urlstring : encodeURI(urlstring));
+ 	this.resource = {};
+ 	this.data = {
+ 		metrics : []
+ 	};
+ 	this.ui = {};
+ 	this.time = {};
+ 	// grab all the non query URL items
+ 	this.resource.host = url.host;
+ 	this.resource.hostname  = url.hostname;
+ 	this.resource.origin = url.origin;
+ 	this.resource.pathname = url.pathname;
+ 	this.resource.port = url.port;
+ 	this.resource.protocol = url.protocol;
+ 	// extract the full query path and decode
+ 	this.query = OpenTSDBURL._decodeQuery(url);
+ 	console.info("Query: [" + this.query + "]");
+ 	if(this.query!="") {
+ 		var splitQuery = this.query.split("&");
+ 		for(var x = 0, y = splitQuery.length; x < y; x++) {
+ 			var pair = OpenTSDBURL._splitByFirst(splitQuery[x], "=");
+ 			console.info("Pair #%s (%s)", x, JSON.stringify(pair));
+ 			if(pair!=null && pair.length > 0) {
+ 				switch(pair[0]) {
+ 					case "start" :
+ 						if(pair.length>1) this.time.start = pair[1];
+ 						break;
+ 					case "m" :
+ 						if(pair.length==1) throw "No metric 'm' value found for query path [" + this.query + "]";
+ 						this._parseMetric(pair[1].replace('"', ''));
+ 						break;
+ 				}
+ 			}
+ 		}
+ 	}
+
 }
 
  /*
@@ -40,6 +140,11 @@
 
 
 /*
+
+Cleaned Query: start=5m-ago&ignore=12295&m=avg:sys.cpu{cpu=*,type=combined,host=PP-WK-NWHI-01}&o=&yrange=[0:]&key=out right top box&wxh=1200x300&png
+
+
+
 URL {hash: "", search: "?start=5m-ago&ignore=275&m=avg:sys.cpu%257Bcpu=*,t…ange=%255B0:%255D&wxh=900x300&smooth=csplines&png", pathname: "/q", port: "8080", hostname: "localhost"…}
 hash: ""
 host: "localhost:8080"
